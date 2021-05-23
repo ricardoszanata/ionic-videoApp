d8d0121e008087cfb578a897ca2d264f0fa907eb
Ricardo da Silva Zanata <euprogramando@euprogramando.com.br>
2021-05-23 19:08:45 -0300

    Adicionado cores novas ao projeto

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +8,35 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+  async confirmarFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Deseja realmente favoritar o FILME?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim, Favoritar!',
+          handler: () => {
+            this.mensagemToast();
+          }
+        }
+      ]
+    });
 
+    await alert.present();
+  }
+  async mensagemToast() {
+    const toast = await this.toastController.create({
+      message: 'Você acabou de Favoritar o FILME.',
+      duration: 2000,
+      color:'primary'
+    });
+    toast.present();
+  }
 }
