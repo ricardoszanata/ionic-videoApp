ce210e0e5be8a68c489697a02d74016b9305a127
Ricardo da Silva Zanata <euprogramando@euprogramando.com.br>
2021-05-30 22:45:00 -0300

    serviço temporário e dados do filme

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 
+
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
@@ -15,4 +16,4 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule { }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,10 @@
+import { DadosService } from './../services/dados.service';
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,11 +12,44 @@ import { ToastController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController, public toastController: ToastController) {}
-  async confirmarFavorito() {
+  titulo = 'Filmes';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura'],
+      pagina: '/mortal-kombat'
+    },
+    {
+      nome: 'Liga da Justiça de Zack Snyder (2021)',
+      lancamento: '18/03/2021',
+      duracao: '4h 2m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
+      generos: ['Ação', 'Aventura', 'Fantasia', 'Ficção científica'],
+      pagina: '/liga-justica'
+    }
+  ];
+
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController,
+    public dadosService: DadosService,
+    public route: Router) { }
+
+  exibirFilme(filme: IFilme) {
+    this.dadosService.guardarDados('filme', filme);
+    this.route.navigateByUrl('/dados-filme');
+  }
+
+  async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
       header: 'Alerta!',
-      message: 'Deseja realmente favoritar o FILME?',
+      message: 'Deseja realmente favoritar o filme?',
       buttons: [
         {
           text: 'Cancelar',
@@ -21,9 +58,9 @@ export class Tab1Page {
             console.log('Confirm Cancel: blah');
           }
         }, {
-          text: 'Sim, Favoritar!',
+          text: 'SIM, favoritar!',
           handler: () => {
-            this.mensagemToast();
+            this.apresentarToast();
           }
         }
       ]
@@ -31,12 +68,14 @@ export class Tab1Page {
 
     await alert.present();
   }
-  async mensagemToast() {
+
+  async apresentarToast() {
     const toast = await this.toastController.create({
-      message: 'Você acabou de Favoritar o FILME.',
+      message: 'Filme adicionado aos favoritos.',
       duration: 2000,
-      color:'primary'
+      color: 'success'
     });
     toast.present();
   }
+
 }
