3d3b8bb7f341940cd1e7239ff7920ec6bfbb96dd
Ricardo da Silva Zanata <euprogramando@euprogramando.com.br>
2021-05-30 23:05:43 -0300

    serviço temporário e dados do filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -32,6 +32,33 @@ export class Tab1Page {
       cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
       generos: ['Ação', 'Aventura', 'Fantasia', 'Ficção científica'],
       pagina: '/liga-justica'
+    },
+    {
+      nome: 'The Matrix (1999)',
+      lancamento: '31/03/1999',
+      duracao: '1h 50m',
+      classificacao: 100,
+      cartaz: 'https://cdn.culturagenial.com/imagens/mv5bnzqzotk3otatndq0zi00ztvklwi0mtetmdllzjnkyznjntc4l2ltywdlxkeyxkfqcgdeqxvynju0otq0oty-v1-cke.jpg',
+      generos: ['Ação', 'Ficção científica'],
+      pagina: '/the-matrix'
+    },
+    {
+      nome: 'The Matrix Reloaded (2003)',
+      lancamento: '16/05/2003',
+      duracao: '1h 50m',
+      classificacao: 100,
+      cartaz: 'https://images-na.ssl-images-amazon.com/images/I/51AhfnN47xL._AC_SY445_.jpg',
+      generos: ['Ação', 'Ficção científica'],
+      pagina: '/the-matrix-reloaded'
+    },
+    {
+      nome: 'The Matrix Revolutions (2003)',
+      lancamento: '05/11/2003',
+      duracao: '1h 50m',
+      classificacao: 100,
+      cartaz: 'https://i.pinimg.com/originals/de/b7/33/deb7335fa11d8cb0e7d0ce6acf4033bc.jpg',
+      generos: ['Ação', 'Ficção científica'],
+      pagina: '/the-matrix'
     }
   ];
 
